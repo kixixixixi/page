5cd02e2b345b41f90c7a5bfd148243ddd4a1cc66
kixixixixi <kixixixixi@gmail.com>
2025-11-04 01:04:48 +0900

    Change base path for prod

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/package-name" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/page" : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
