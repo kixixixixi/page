ccc972085fa3c941a5518719d028ab4003676a5e
kixixixixi <kixixixixi@gmail.com>
2025-11-21 15:21:42 +0900

    Add header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "reset.css"
+import "./reset.css"
 
 export const metadata = {
   title: "Page title",
@@ -9,7 +9,41 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
-        <main>{children}</main>
+        <header
+          style={{
+            backgroundColor: "#f9f9f9",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            padding: ".5rem",
+            position: "relative",
+          }}
+        >
+          <h1
+            style={{
+              fontSize: "1rem",
+              margin: 0,
+            }}
+          >
+            Page title
+          </h1>
+        </header>
+        <main
+          style={{
+            background: "#f0f0f0",
+            minHeight: "calc(100dvh - 3rem)",
+            padding: "1rem",
+          }}
+        >
+          {children}
+        </main>
+        <footer
+          style={{
+            backgroundColor: "#f9f9f9",
+            fontSize: ".75rem",
+            padding: "1rem",
+          }}
+        >
+          <p>&copy; My organization</p>
+        </footer>
       </body>
     </html>
   )
