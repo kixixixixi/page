33966cb9c84140ee23a2878dec32b3a2a05f7526
kixixixixi <kixixixixi@gmail.com>
2026-02-13 15:59:19 +0900

    Update dependency and migrate eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,15 @@ import tseslint from "typescript-eslint"
 import pluginReact from "eslint-plugin-react"
 import pluginReactHooks from "eslint-plugin-react-hooks"
 import jsxA11y from "eslint-plugin-jsx-a11y"
+import { fixupConfigRules } from "@eslint/compat"
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { ignores: [".next", "next-env.d.ts", "next.config.js"] },
+  { ignores: [".next", "out", "next-env.d.ts", "next.config.js"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  jsxA11y.flatConfigs.recommended,
+  ...fixupConfigRules(pluginReact.configs.flat.recommended),
+  ...fixupConfigRules(jsxA11y.flatConfigs.recommended),
   {
     plugins: {
       "react-hooks": pluginReactHooks,
