4119b9ff3f712f78e083b76a98d4ea665685089d
kixixixixi <kixixixixi@gmail.com>
2026-08-10 12:33:41 +0900

    Improve component styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Title } from "@/components/elements/layout"
+import { Footer, Header, Main, Title } from "@/components/elements/layout"
 import "./reset.css"
 
 export const metadata = {
@@ -10,35 +10,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
-        <header
-          style={{
-            backgroundColor: "#333",
-            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-            padding: ".5rem 1rem",
-            position: "relative",
-          }}
-        >
+        <Header>
           <Title>Page title</Title>
-        </header>
-        <main
-          style={{
-            background: "#222",
-            minHeight: "calc(100dvh - 5.625rem)",
-            padding: "1rem",
-          }}
-        >
-          {children}
-        </main>
-        <footer
-          style={{
-            backgroundColor: "#333",
-            boxShadow: "0 -4px 6px rgba(0, 0, 0, 0.1)",
-            fontSize: ".75rem",
-            padding: "1rem",
-          }}
-        >
+        </Header>
+        <Main>{children}</Main>
+        <Footer>
           <p>&copy; My organization</p>
-        </footer>
+        </Footer>
       </body>
     </html>
   )
